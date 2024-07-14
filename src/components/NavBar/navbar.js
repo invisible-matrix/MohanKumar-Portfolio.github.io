7ced6a317dead19ed378cb6def1eed773afe76de
invisible-matrix <70465572+invisible-matrix@users.noreply.github.com>
2024-07-14 11:55:17 +0530

    Add files via upload

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <img src="" alt="" />
+            <div className='.desktopMenu'>
+
+            </div>
+            <button className='desktopMenuBtn'>
+                <img src='' className='desktopMenuImg' />Contact Me
+            </button>
+        </nav>
+    )
+}
+
+export default Navbar
